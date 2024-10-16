3f6ca8d9ad9dbe08dd4f170eeb77b10578a2b8f3
Oleh Mazur bc2024-4 <olegmazur541@gmail.com>
2024-10-16 18:37:01 +0300

    Server init

--- src/mian.js
+++ src/mian.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const {Command} = require("commander");
 const fs = require('fs');
 const path = require("node:path");
@@ -5,11 +6,51 @@ const path = require("node:path");
 let program = new Command();
 
 program
-    .option('-i, --input <string>', 'Input file path')
-    .option('-o, --output <string>', 'Output file path')
-    .option('-d, --display', 'Display result')
-    .option('-b, --debug', 'Show debug info');
+    .option('-h, --host <adress>', 'Input file path')
+    .option('-p, --port <port>', 'Output file path')
+    .option('-c, --сache <path>', 'Display result')
 
 program.parse(process.argv);
 
 const options = program.opts();
+
+if (!options.host || !options.port) {
+    console.error("Specify port and address correctly")
+    process.exit(1);
+}
+
+const hostname = options.host; // or 'localhost'
+const port = Number(options.port);
+
+const getRequestBody = (req) => {
+    return new Promise((resolve, reject) => {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            resolve(body);
+        });
+        req.on('error', (err) => {
+            reject(err);
+        });
+    });
+};
+
+const server = http.createServer(async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    if (req.method === 'GET') {
+         res.statusCode = 200;
+         res.end("req");
+    } else if (req.method === 'PUT') {
+    } else if (req.method === 'DELETE') {
+    } else {
+        res.statusCode = 405;
+        res.end(JSON.stringify({message: 'Method not allowed'}));
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
